7c5e83ba272d31f6d75448f3bbb64343b28425c5
s1llyMonke <cbimutovic121@gmail.com>
2024-12-14 16:35:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2496,7 +2496,7 @@ Game.Launch=function()
 		
 		Game.grandmaNames=['Granny','Gusher','Ethel','Edna','Doris','Maud','Hilda','Gladys','Michelle','Michele','Phyllis','Millicent','Muriel','Myrtle','Mildred','Mavis','Helen','Gloria','Sheila','Betty','Gertrude','Agatha','Beryl','Agnes','Pearl','Precious','Ruby','Vera','Bonnie','Ada','Bunny','Cookie','Darling','Gaga','GamGam','Memaw','Mimsy','Peanut','Nana','Nan','Tootsie','Warty','Stinky','Heinous'];
 		Game.customGrandmaNames=[];
-		Game.heralds=0;
+		Game.heralds=10000;
 		
 		Game.GrabData=function()
 		{
