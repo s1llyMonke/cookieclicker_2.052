09c47bbb749a71784d5753712910db07c7f79002
s1llyMonke <cbimutovic121@gmail.com>
2025-01-24 14:31:12 -0500

    Added CookieMonster plugin

--- main.js
+++ main.js
@@ -2283,7 +2283,7 @@ Game.Launch=function()
 		AddEvent(Game.bakeryNameL,'click',Game.bakeryNamePrompt);
 		
 		Game.bakeryNameSet(Game.GetBakeryName());
-		Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
+		Game.LoadMod("cookiemonster.js");
 		
 		/*=====================================================================================
 		TOOLTIP
